421d62cf582a38d2f91867a2f6e3cd0939ae1df3
Louis-Vincent <louis-vincent@triton.one>
2026-08-21 13:48:03 -0400

    clickhouse-sink: ingest entries info + txn index

--- apps/jet-txn-landing-elt/clickhouse/permissions.sql
+++ apps/jet-txn-landing-elt/clickhouse/permissions.sql
@@ -1,3 +1,5 @@
 
 grant select, insert on jet.chain_transaction_staging to 'jet-clickhouse-sink';
 grant select, insert on jet.sent_transaction_pending to 'jet-clickhouse-sink';
+grant select, insert on jet.txn_trace to 'jet-clickhouse-sink';
+grant select, insert on jet.chain_entry_staging to 'jet-clickhouse-sink';
